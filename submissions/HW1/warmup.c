6b6e521c058c52a88f98dc8829c88f75a60e8f5d
George Price Clark <gpwclark@classroom.cs.unc.edu>
2015-08-27 23:43:03 -0400

    finally working, but needs comments

--- submissions/HW1/warmup.c
+++ submissions/HW1/warmup.c
@@ -7,13 +7,12 @@
  * Step 5. Modify code to replace pair of asterisks with ^
  */ 
 
-#include <stdint.h>                                                        
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdbool.h>
 
-#define LINE_LENGTH 80
+#define LINE_LENGTH 10
 
 int main() {
   int input_char;
@@ -23,11 +22,11 @@ int main() {
   if(buffer == NULL){
     return -1;
   }
-  
+
   int count = 0;
   do{
 
-    input_char = getchar();
+    input_char = fgetc(stdin);
 
     if(isprint(input_char) || isspace(input_char)){
 
@@ -37,32 +36,52 @@ int main() {
       if(input_char == '*'){
         asterisk_encountered = true;
       }
-      
+
       if(count < LINE_LENGTH){
         if(asterisk_encountered && (count != 0) && (buffer[count-1] == '*') ){
           buffer[count-1] = '^'; 
           --count;
           asterisk_encountered = false;
-        }else{
+        }
+
+        else{
           buffer[count] = input_char;
-       }
+        }
 
       }
-      
+
       ++count;
 
       if(count == LINE_LENGTH){
-        for(int i = 0; i < LINE_LENGTH; ++i){
+
+        if(buffer[LINE_LENGTH-1] == '*'){
+          int temp_char = fgetc(stdin);
+          if(temp_char == '*'){
+            buffer[LINE_LENGTH-1] = '^';
+          }else{
+            int ret_val = ungetc(temp_char,stdin);
+
+            // If the return value is EOF then the stream is broken so I
+            // free the buffer and exit with an error code.
+            if(ret_val == EOF){
+              free(buffer);
+              return -1;
+            }
+          }
+        }
+
+        int i;
+        for(i = 0; i < LINE_LENGTH; ++i){
           fprintf(stdout, "%c", buffer[i]);
         }
         printf("\n");
         count = 0;
+        memset(buffer, 0, LINE_LENGTH*sizeof(int));
       }
-  
+
     }
 
   }while(input_char != EOF);
-  
 
   free(buffer);
 
