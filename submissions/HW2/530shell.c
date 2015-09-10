06b1894b2a9c397028bd21574331ad7d12772891
gpwclark <gpwclark@gmail.com>
2015-09-10 11:22:46 -0400

    program now reliably reads lines and terminates when it needs to

--- submissions/HW2/530shell.c
+++ submissions/HW2/530shell.c
@@ -2,13 +2,44 @@
 #include "AllTests.h"
 #include "string.h"
 #include "stdio.h"
+#include "errno.h"
 
 //TODO make shell prompt a constant?
+//TODO meaningful error messages
+//TODO does shell_prompt print one extra time?
 
 int runShell() {
   char *shell_prompt = "% ";
-  printf("%s",shell_prompt);
-  return 0;
+  char *line;
+  size_t chars_to_read = 0;
+  int chars_read;
+  errno = 0; 
+
+  int input_char;
+
+  do {
+    printf("%s",shell_prompt);
+    chars_read = getline(&line, &chars_to_read, stdin);
+    
+    if (chars_read != -1){
+      puts(line);
+    }else{
+      if (errno != 0){
+        perror("Failed to read input line");
+      }
+      break;
+    }
+    
+    
+
+  } while (input_char != EOF);
+  free(line);
+  
+  if (errno == 0){
+    return 0;
+  }else{
+    return -1;
+  }
 }
 
 void TestStrToUpper(CuTest *tc) {
@@ -25,14 +56,14 @@ CuSuite* ShellGetSuite() {
 }
 
 int main(int argc, char **argv) {
+  int ret_val = 0;
 
   if ( (argc == 2) && (strcmp(argv[1], "--test") == 0) ){
     RunAllTests();
     
   }else {
-    runShell();
-
+    ret_val = runShell();
   }
 
-  return 0;
+  return ret_val;
 }
