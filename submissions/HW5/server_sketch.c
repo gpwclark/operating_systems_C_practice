f778b6564bfae819c179f10f0ca8dc5e7f0829a9
gpwclark <gpwclark@gmail.com>
2015-10-23 21:52:27 -0400

    my first segfault! just added PID stuff and now it has broken

--- submissions/HW5/server_sketch.c
+++ submissions/HW5/server_sketch.c
@@ -1,3 +1,5 @@
+// I HAVE NEITHER GIVEN NOR RECEIVED HELP ON THIS PROGRAM - George Clark 720063665
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,6 +17,92 @@
 // TODO remove NUM_ARGS
 // TODO implement the socket
 // TODO free memory
+// TODO check for length of max pid!!
+
+int parse_args(char *raw_arg_string, char **string_array){
+  char *token;
+  char white_space_delim[] = " \t\n\v\f\r";
+  char *new_string = strdup(raw_arg_string);
+  token = strtok(new_string, white_space_delim);
+
+  int i = 0;
+  // Loop terminates when the token strtok parses evaluates to NULL,
+  // indicating that the line of input is done parsing.
+  while( (token != NULL) && (i < MAX_ARGS)){ 
+    string_array[i] = token; 
+    token = strtok(NULL, white_space_delim);
+    ++i;
+  }
+
+  if(i < MAX_ARGS){
+    return 0;
+  }else{
+    return -1;
+  }
+}
+
+int run_command(char* cmd_to_run, FILE* tmp_fp){
+  pid_t child_PID;
+  int status;
+  errno = 0;
+  child_PID = fork();
+
+  if (child_PID < 0){
+    // no child process created, fork failed
+    perror("No child process, failed to fork");
+  } else if (child_PID == 0){
+    // printf("I am the child. My childPID is %ld\n", (long)child_PID);
+    errno = 0;
+    char** string_array = (char**) malloc (MAX_ARGS * sizeof(char*));
+    if(string_array == NULL){
+      perror("Child's malloc failed for constructing the argument list");
+      abort();
+    }
+
+    int ret_val = parse_args(cmd_to_run, string_array); 
+    if(ret_val == -1){
+      //ERROR, LINE EXCEEDS MAX LENGTH
+      printf("%s%d\n","Line exceeds max number of arguments: ", MAX_ARGS - 1);
+      abort();
+    }
+
+    errno = 0;
+    int exec_return = execvp(*string_array, string_array);
+    if (exec_return < 0) {
+      perror("Failed to execute, error with command as inputted");
+      fclose(stdout); 
+      abort();
+
+    }
+
+    free(string_array);
+
+  } else {
+    //we are the parent
+    //printf("I am the parent. My child's PID is %ld\n", (long)child_PID);
+
+    errno = 0; 
+    child_PID = wait(&status);
+    if (child_PID == -1){ //Wait for child process.
+      perror("wait error");
+
+    } else { 
+
+      if (WIFSIGNALED(status) != 0){
+        printf("%s\n","Child process terminated because of receipt of signal.");
+      } else if (WIFEXITED(status) != 0){
+        // printf("%s\n", "Child process terminated as expected.");
+      } else if(WIFSTOPPED(status != 0)){
+        printf("%s\n", "Child process stopped.");
+      } else{
+        printf("%s\n", "Child process terminated with error.");
+      }
+
+    }
+    //printf("%s\n","Parent process ended.");
+
+  }
+}
 
 char* get_tmp_file_name(){
 
@@ -79,6 +167,7 @@ int main(){
     ++i;
   }
 
+  printf("%s\n", "not here");
   //Pattern: command, if(failure)(do_this) else (do_that)
 
   // first command
@@ -104,10 +193,12 @@ int main(){
       while (i < NUM_ARGS){
         // fourth command
         //TODO get actual error? depends on my hw2 program
-        int ret_val = run_command(cmd_string, fd, conn, ps_string);
+        int ret_val = run_command(cmd_array[i], fp);
         if (ret_val == -1){
-          error = "command failed\n";
-          printf(error);
+          //error = "command failed\n";
+          //printf(error);
+          //TODO the command failed, we pass the error to client via
+          // what is written to stdout?
         } else {
           /*
           //this is where we end up when everything went well
