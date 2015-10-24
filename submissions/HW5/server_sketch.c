540beadc37307e475674153356d5949161984d84
gpwclark <gpwclark@gmail.com>
2015-10-23 21:27:49 -0400

    working implmentation of dummy server up to command parsing

--- submissions/HW5/server_sketch.c
+++ submissions/HW5/server_sketch.c
@@ -3,11 +3,30 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
+#define TMP_FILE_PREFIX_LEN 3
+#define MAX_PID_LEN 5
+#define MAX_ARGS 100
 #define NUM_ARGS 6
 
 // TODO obviously, the error printing needs to turn into a send
+// TODO remove NUM_ARGS
+// TODO implement the socket
 // TODO free memory
+
+char* get_tmp_file_name(){
+
+  char* tmp_file_name = (char*) malloc ((TMP_FILE_PREFIX_LEN + MAX_PID_LEN) * sizeof(char));
+  int ret_val = sprintf(tmp_file_name, "tmp%d", getpid());
+
+  if (ret_val < 0){
+    return NULL;
+  } else {
+    return tmp_file_name;
+  }
+}
 
 char** accept_connection(){
 
@@ -17,6 +36,7 @@ char** accept_connection(){
   ssize_t read;
 
   char** string_array = (char**) malloc (NUM_ARGS * sizeof(char*));
+
 
   fp = fopen("cmd_file", "r");
   if (fp == NULL){
@@ -42,8 +62,7 @@ char** accept_connection(){
 }
 
 int main(){
-  //What's up with C strings?
-  //  char[] error = "";
+  char error[200];
   //  char[] ps_string = "";
   //  char[] cmd_string = "";
 
@@ -55,7 +74,7 @@ int main(){
   char **cmd_array = accept_connection();
   printf("returned string array:\n");
   int i = 0;
-  while (i < 6){
+  while (i < MAX_ARGS){
     printf("%s", cmd_array[i]);
     ++i;
   }
@@ -63,50 +82,48 @@ int main(){
   //Pattern: command, if(failure)(do_this) else (do_that)
 
   // first command
-  ps_string = get_ps_id_string(); //tmpxxxx
-  if (ps_string == ""){
-    error = "failed to construct ps_id_string\n";
-    send(error);
+  //TODO free this
+  char *tmp_file_name = get_tmp_file_name(); //tmpxxxx
+  if (tmp_file_name == NULL){
+    sprintf(error, "Failed to construct tmp_file_name_string\n");
+    printf(error);
   } else {
-    printf(ps_string);
+    printf("%s\n", tmp_file_name);
 
-    /*
     // second command
-    int fd = freopen(ps_string, 'w+',stdout);
-    if(fd == fail){
-    error = "failed to create tmp file for stdout\n";
-    send(error);
+    errno = 0;
+    FILE *fp = freopen(tmp_file_name, "w+", stdout);
+    if(fp == NULL){
+      sprintf(error, "Failed to redirect stdout to tmp file: %s\n", strerror(errno));
+      printf(error);
     } else {
 
-// third command
-cmd_string = conn.get_line();
-if (cmd_string == ""){
-error = "failed to get string from connection\n";
-send(error);
-} else {
-while (cmd_string != EOF) {
+      // third command, will this be necessary when we implement socket?
+      // something like string = conn.getLine();
+      int i = 0;
+      while (i < NUM_ARGS){
+        // fourth command
+        //TODO get actual error? depends on my hw2 program
+        int ret_val = run_command(cmd_string, fd, conn, ps_string);
+        if (ret_val == -1){
+          error = "command failed\n";
+          printf(error);
+        } else {
+          /*
+          //this is where we end up when everything went well
 
-  // fourth command
-  //TODO get actual error? depends on my hw2 program
-  int ret_val = run_command(cmd_string, fd, conn, ps_string);
-  if (ret_val == -1){
-  error = "command failed\n";
-  send(error);
-  } else {
-    //this is where we end up when everything went well
-
-    }
-    cmd_string = conn.get_line();
-    if (cmd_string == ""){
-    error = "failed to get string from connection\n";
-    printf("%s\n", error);
-    break;
-    }
+          }
+          cmd_string = conn.get_line();
+          if (cmd_string == ""){
+          error = "failed to get string from connection\n";
+          printf("%s\n", error);
+          break;
+          }
+          */
+      }
     } // end while
-    }
-    }
-    */
-  } 
+  }
+} 
 
 // TODO do not forget to remove the temp file
 // if (fd != fail)
