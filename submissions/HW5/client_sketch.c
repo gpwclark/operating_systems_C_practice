9a38b20242375abb048f8f25129661ff43a9cd86
gpwclark <gpwclark@gmail.com>
2015-10-25 14:44:53 -0400

    about ready to start implementing socket code

--- submissions/HW5/client_sketch.c
+++ submissions/HW5/client_sketch.c
@@ -1,2 +1,72 @@
 // I HAVE NEITHER GIVEN NOR RECEIVED HELP ON THIS PROGRAM - George Clark 720063665
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 
+//TODO remove useless includes in server and client
+
+int parse_response(){
+ //read_in_response.
+ // Need to distinguish between 3 different cases.
+ //   1. FATAL error, server can't do a thing so we ahve to abort
+ //     entirely.
+ //   2. and 3.
+ //     now, because the server is writing all none fatal errors
+ //     to the socket, and an error will just be one line (theoretically)
+ //     and a normal response will be w/e cmd output is followed by
+ //     a response line, it doesn't seem like I even need to freaking
+ //     worry about differentiating between 2 and 3.
+
+  return 0;
+}
+
+int run_shell() {
+//TODO need to decide when to get response and how to print it.
+  static const char SHELL_PROMPT[] = "% ";
+  char *line;
+  size_t chars_to_read = 0;
+  int chars_read;
+  errno = 0; 
+
+  int input_char;
+
+  /*
+   * This do while loop terminates when the return value for getline is -1, 
+   * indicating that the getline function has encountered the EOF.
+   */
+  do {
+    printf("%s",SHELL_PROMPT);
+
+    errno = 0;
+    chars_read = getline(&line, &chars_to_read, stdin);
+
+    int getline_failure = 0;
+    if (errno < 0){
+      perror("Failed to read input line");
+      getline_failure = -1;
+    }
+    
+
+    //TODO HERE we need to send the line to the server, but I think we also
+    //need to make sure it isn't EOF. if it is send that to server for shutdown
+    //but then break from this loop.
+    //
+  } while (chars_read != -1);
+  free(line);
+  //TODO close socket.
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  int ret_val = 0;
+  //TODO get host and port from argc and argv
+  //TODO init the socket.
+
+  ret_val = run_shell();
+  //TODO close the socket?
+  return ret_val;
+
+}
--- submissions/HW5/server_sketch.c
+++ submissions/HW5/server_sketch.c
@@ -18,6 +18,7 @@
 // TODO implement the socket
 // TODO free memory
 // TODO check for length of max pid!!
+// TODO COMMENT YOUR CODE and check that thing about loops and invariants and stuff.
 
 int parse_args(char *raw_arg_string, char **string_array){
   char *token;
@@ -49,13 +50,15 @@ int run_command(char* cmd_to_run, FILE* tmp_fp){
 
   if (child_PID < 0){
     // no child process created, fork failed
-    perror("No child process, failed to fork");
+    printf("No child process, failed to fork: %s\n", strerror(errno));
+    //TODO send error to client
   } else if (child_PID == 0){
     // printf("I am the child. My childPID is %ld\n", (long)child_PID);
     errno = 0;
     char** string_array = (char**) malloc (MAX_ARGS * sizeof(char*));
     if(string_array == NULL){
-      perror("Child's malloc failed for constructing the argument list");
+      printf("Child's malloc failed for constructing the argument list: %s\n", strerror(errno));
+    //TODO send error to client
       abort();
     }
 
@@ -63,14 +66,15 @@ int run_command(char* cmd_to_run, FILE* tmp_fp){
     if(ret_val == -1){
       //ERROR, LINE EXCEEDS MAX LENGTH
       printf("%s%d\n","Line exceeds max number of arguments: ", MAX_ARGS - 1);
+      //TODO send error to client
       abort();
     }
 
     errno = 0;
     int exec_return = execvp(*string_array, string_array);
     if (exec_return < 0) {
-      perror("Failed to execute, error with command as inputted");
-      fclose(stdout); 
+      printf("Failed to execute, error with command as inputted: %s\n", strerror(errno));
+      //TODO send error to client
       abort();
 
     }
@@ -84,9 +88,12 @@ int run_command(char* cmd_to_run, FILE* tmp_fp){
     errno = 0; 
     child_PID = wait(&status);
     if (child_PID == -1){ //Wait for child process.
-      perror("wait error");
+      printf("Wait error: %s\n", strerror(errno));
 
     } else { 
+      //TODO we only need to look at WIFEXITED and WIFEXITSTATUS
+      //WIFEXITED always gets sent, if it is non-zero then we also
+      //send WIFEXITSTATUS.
 
       if (WIFSIGNALED(status) != 0){
         printf("%s\n","Child process terminated because of receipt of signal.");
@@ -97,15 +104,23 @@ int run_command(char* cmd_to_run, FILE* tmp_fp){
       } else{
         printf("%s\n", "Child process terminated with error.");
       }
+      //TODO Need to use fopen() and read() on tmp_file_name to read what came 
+      //in on stdout, as of now, unsure how this works IF there was some sort
+      //of error, also, need to overwrite the file so we don't report old stuff?
+      //We also have to send that response line...
 
     }
     //printf("%s\n","Parent process ended.");
 
-  }
+  } //end parent
+  //TODO we could return -1 on error but it might not make sense since 
+  //regardless of error we just send stdout to server.
+  //TODO we could return -1 on error but it might not make sense since 
+  //regardless of error we just send stdout to server.
+  return 0;
 }
 
 char* get_tmp_file_name(){
-
   char* tmp_file_name = (char*) malloc ((TMP_FILE_PREFIX_LEN + MAX_PID_LEN) * sizeof(char));
   int ret_val = sprintf(tmp_file_name, "tmp%d", getpid());
 
@@ -117,7 +132,6 @@ char* get_tmp_file_name(){
 }
 
 char** accept_connection(){
-
   FILE *fp;
   char *line = NULL;
   size_t len = 0;
@@ -149,7 +163,7 @@ char** accept_connection(){
   return string_array;
 }
 
-int main(){
+int main(int argc, char **argv) {
   char error[200];
   //  char[] ps_string = "";
   //  char[] cmd_string = "";
@@ -159,31 +173,35 @@ int main(){
   // in the file will be a command to be executed.
   // conn = socket();
 
+  //TODO free this
   char **cmd_array = accept_connection();
+  //TODO send error straight to client rather than printing.
   printf("returned string array:\n");
   int i = 0;
-  while (i < MAX_ARGS){
+  while (i < NUM_ARGS){
     printf("%s", cmd_array[i]);
     ++i;
   }
 
-  printf("%s\n", "not here");
   //Pattern: command, if(failure)(do_this) else (do_that)
 
   // first command
   //TODO free this
   char *tmp_file_name = get_tmp_file_name(); //tmpxxxx
   if (tmp_file_name == NULL){
+    //TODO send error straight to client rather than printing.
     sprintf(error, "Failed to construct tmp_file_name_string\n");
     printf(error);
   } else {
+    //TODO we don't actually want to print this...
     printf("%s\n", tmp_file_name);
 
     // second command
     errno = 0;
     FILE *fp = freopen(tmp_file_name, "w+", stdout);
     if(fp == NULL){
-      sprintf(error, "Failed to redirect stdout to tmp file: %s\n", strerror(errno));
+      //TODO send error straight to client rather than printing.
+      sprintf(error,"Failed to redirect stdout to tmp file: %s\n", strerror(errno));
       printf(error);
     } else {
 
@@ -195,25 +213,28 @@ int main(){
         //TODO get actual error? depends on my hw2 program
         int ret_val = run_command(cmd_array[i], fp);
         if (ret_val == -1){
+          printf("the command did not work");
           //error = "command failed\n";
           //printf(error);
           //TODO the command failed, we pass the error to client via
-          // what is written to stdout?
+          // what is written to stdout? or
+          //TODO wouldn't the client handle what kind of error this is?
+          //and sending it? Perhaps we should handle all of the sending
+          //to the client in this main function and the client will just pass
+          //the message directly or via some file?
         } else {
+          
           /*
           //this is where we end up when everything went well
-
-          }
-          cmd_string = conn.get_line();
-          if (cmd_string == ""){
-          error = "failed to get string from connection\n";
-          printf("%s\n", error);
-          break;
-          }
+          //TODO this means here we need to read the tmp file
+          //and send something to the client
           */
+        ++i;
       }
     } // end while
   }
+  free(cmd_array);
+  free(tmp_file_name);
 } 
 
 // TODO do not forget to remove the temp file
