d20850ac618e1dd5ad42f829336c199a3b162d96
gpwclark <gpwclark@gmail.com>
2015-10-26 15:46:08 -0400

    Server sketch implementation seems to be working perfectly, ready for some sockets

--- submissions/HW5/server_sketch.c
+++ submissions/HW5/server_sketch.c
@@ -10,7 +10,9 @@
 #include <sys/types.h>
 
 #define TMP_FILE_PREFIX_LEN 3
+#define RESPONSE_STR_LEN 280
 #define ERROR_STR_LEN 140
+#define READ_BUF_SIZE 10
 #define MAX_PID_LEN 5
 #define MAX_ARGS 100
 #define NUM_ARGS 6
@@ -46,14 +48,14 @@ int parse_args(char *raw_arg_string, char **string_array){
   }
 }
 
-int run_command(char* cmd_to_run, FILE* tmp_fp){
+int run_command(char* cmd_to_run, char* tmp_file_name){
   pid_t child_PID;
   int status;
   errno = 0;
   child_PID = fork();
   char error_string[ERROR_STR_LEN];
   bool error_found = false;
-//TODO make sure final RESPONSE line has everything we need.
+  //TODO make sure final RESPONSE line has everything we need.
   if (child_PID < 0){
     // no child process created, fork failed
     sprintf(error_string, "No child process, failed to fork: %s", strerror(errno));
@@ -93,53 +95,103 @@ int run_command(char* cmd_to_run, FILE* tmp_fp){
     //TODO comment in video about WNOHANG?
     errno = 0; 
     child_PID = waitpid(child_PID, &status, WCONTINUED);
-    if (child_PID == -1){ //Wait for child process.
-      sprintf(error_string, "Wait error: %s\n", strerror(errno));
+    if (child_PID == -1){ //Wait for child process.  
+      sprintf(error_string, "Wait error: %s\n", strerror(errno)); 
       error_found = true;
     } else { 
-      //TODO we only need to look at WIFEXITED and WIFEXITSTATUS
-      //WIFEXITED always gets sent, if it is non-zero then we also
-      //send WIFEXITSTATUS.
-
       /*
-      if (WIFSIGNALED(status) != 0){
-        printf("%s\n","Child process terminated because of receipt of signal.");
-      } else if (WIFEXITED(status) != 0){
+         if (WIFSIGNALED(status) != 0){
+         printf("%s\n","Child process terminated because of receipt of signal.");
+         } else if (WIFEXITED(status) != 0){
         // printf("%s\n", "Child process terminated as expected.");
-      } else if(WIFSTOPPED(status != 0)){
+        } else if(WIFSTOPPED(status != 0)){
         printf("%s\n", "Child process stopped.");
-      } else{
+        } else{
         printf("%s\n", "Child process terminated with error.");
-      }
+        }
       */
       int wif_exited = WIFEXITED(status);
+      int exit_status;
       if (wif_exited != 0){
-        // printf("%s\n", "Child process terminated as expected.");
-        fprintf(stderr, "wifexit: %d\n", wif_exited);
-      } else {
-        int exit_status = WEXITSTATUS(status); 
-        fprintf(stderr, "wifexit: %d :: wifstatus: %d\n", wif_exited, exit_status);
+        exit_status = WEXITSTATUS(status); 
       }
 
       //TODO Need to use fopen() and read() on tmp_file_name to read what came 
       //in on stdout, as of now, unsure how this works IF there was some sort
       //of error, also, need to overwrite the file so we don't report old stuff?
       //We also have to send that response line...
+      if(!error_found) {
+        // Need to fopen and read tmp file. 
+        // but also when we are done reading it we need to clear it so
+        // we don't send duplicate data next time.
+        FILE *fp;
+
+        errno = 0;
+        fp = fopen(tmp_file_name, "r");
+        if (fp == NULL){
+          sprintf(error_string, "Failed to open file with stdout for reading: %s", strerror(errno));
+          error_found = true;
+        } else {
+          int fd = fileno(fp);
+          char buf[READ_BUF_SIZE];
+          int num_bytes_read;
+          // successful reads
+          // partial reads
+          // EOF, ret val is -1 but errno is 0
+          // IO error, ret val is -1 and errno is != 0
+
+          errno = 0;
+          do {
+            num_bytes_read = read(fd, buf, READ_BUF_SIZE);
+
+            if (num_bytes_read == -1){
+              if (errno != 0){
+                sprintf(error_string, "Error while attempting to read file with stdout from exec comand: %s", strerror(errno));
+                error_found = true;
+                //TODO send error, file IO
+              } else {
+                //EOF reached
+                break;
+              }
+            }
+
+            for(int i = 0; i < num_bytes_read; i++) {
+              fprintf(stderr, "%c", buf[i]);
+              //TODO this gets sent to the client
+              //we sort of want to put it in a buffer though, just in case 
+              //clearing the file fails.
+            }
+
+          } while (num_bytes_read != 0);
+
+          FILE *fp = fopen(tmp_file_name, "w");
+          if (fp == NULL){
+            sprintf(error_string, "Failed to clear contents of tmp file: %s", strerror(errno));
+            error_found = true;
+          }
+        }
+      }
+      
+      char response_string[RESPONSE_STR_LEN];
+
       if(error_found){
         //TODO
         //response needs wifexited and wexitstatus,
         //response is just RESPONSE: error_string wifexited and wexitstatus.
         //This will be a generic response shared by fatal error
-
+        sprintf(response_string, "%s: %s\n", "RESPONSE", error_string);
+        fprintf(stderr, "%s", response_string);
       } else {
-        // Need to fopen and read tmp file. 
-        // but also when we are done reading it we need to clear it so
-        // we don't send duplicate data next time.
         // then we send response 
         // response needs wifexited and wexitstatus
         // RESPONSE: End of Comand's stdout: exit_code: %d, exit_status: %d
-
-        //TODO what if error occurs here... send an error response I guess
+        //TODO send normal response
+        if (wif_exited != 0){
+          sprintf(response_string, "%s: %s, Exit Code: %d, Exit Status: %d\n", "RESPONSE", "End of stdout", wif_exited, exit_status);
+        } else {
+          sprintf(response_string, "%s: %s, Exit Code: %d\n", "RESPONSE", "End of stdout", wif_exited);
+        }
+        fprintf(stderr, "%s", response_string);
       }
 
     }
@@ -246,7 +298,7 @@ int main(int argc, char **argv) {
       while (i < NUM_ARGS){
         // fourth command
         //TODO get actual error? depends on my hw2 program
-        int ret_val = run_command(cmd_array[i], fp);
+        int ret_val = run_command(cmd_array[i], tmp_file_name);
         if (ret_val == -1){
           printf("the command did not work");
           //error = "command failed\n";
@@ -258,17 +310,17 @@ int main(int argc, char **argv) {
           //to the client in this main function and the client will just pass
           //the message directly or via some file?
         } else {
-          
+
           /*
           //this is where we end up when everything went well
           //TODO this means here we need to read the tmp file
           //and send something to the client
           */
-        ++i;
-      }
-    } // end while
-  }
-} 
+          ++i;
+        }
+      } // end while
+    }
+  } 
 
   if(error_found){
     //TODO
